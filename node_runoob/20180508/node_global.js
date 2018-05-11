41605e29b03f0000c39aee66f019e494e0dd8330
霍超群 <2440751123@qq.com>
2018-05-11 09:12:21 +0800

    node test

--- node_runoob/20180508/node_global.js
+++ node_runoob/20180508/node_global.js
@@ -3,4 +3,51 @@
 
 console.log(__filename);
 console.log(__dirname);
-console.log();
+console.trace();
+
+// process: global.process
+
+process.on('beforeExit', function () {
+    console.log(arguments);
+    console.log('beforeExit');
+})
+
+process.on('exit', function () {
+    console.log(arguments);
+    console.log('exit');
+})
+
+process.on('Signal', function () {
+    console.log(arguments);
+    console.log('Signal');
+})
+
+process.on('uncaughtException', function () {
+    console.log(arguments);
+    console.log('uncaughtException');
+})
+
+// console.log(process.env);
+
+// console.log(process.versions);
+// console.log(process.config);
+// console.log(process.pid);
+// console.log(process.arch);
+// console.log(process.platform);
+// console.log(process.stdout.write('Hello node global'));
+// console.log(process.stderr.write('hhhh'));
+// console.log(process.stdin.read());
+// console.log(process.stdin.argv);
+
+
+console.log(process.cwd())
+// process.abort();
+
+// process.exit(2)
+
+// console.log(process.kill(process.getgid()));
+console.log(process.getgid());
+console.log(process.getuid());
+console.log(process.getgroups());
+console.log(process.memoryUsage());
+console.log(process.uptime());
