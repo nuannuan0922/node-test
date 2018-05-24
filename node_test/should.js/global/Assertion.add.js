5fca76aec4e91312ad5d91fc56e354851388f1ca
霍超群 <2440751123@qq.com>
2018-05-24 23:01:44 +0800

    node should.js

--- node_test/should.js/global/Assertion.add.js
+++ node_test/should.js/global/Assertion.add.js
@@ -73,6 +73,11 @@ obj.should.is.not.an.instanceof(Array).and.which.have.property('childs').with.le
 /**
  * this in assertion function will be instance of should.Assertion 
  * and you must define in any way this.params object in your assertion function call before assertion check happen.
+ * 
+ * There you can see that assertion function do not define own this.params and 
+ * instead call within the same assertion .exactly that will fill this.params.
+ *  You should use this way very carefully, 
+ * but you can use it.
  */
 
  should.Assertion.add('huo', function () {
@@ -90,3 +95,61 @@ obj.should.is.not.an.instanceof(Array).and.which.have.property('childs').with.le
 
  obj.should.not.be.an.huo()
  obj_huo.should.be.an.huo();
+
+ obj_huo.should.any.huo();
+//  obj_huo.should.not.any.huo();
+// Object.keys(obj_huo).only.property('id')
+
+var obj_id = {
+    id: 3
+}
+Object.keys(obj_id).should.have.an.property('0', 'id')
+
+var obj_format = {
+    id: 4,
+    name: '4'
+}
+// console.log(should.format(obj_format));
+// console.log(should.parse(should.format(obj_format)));
+
+var obj_4 = {
+    id: 4,
+    name: '4'
+}
+
+should(obj_format).deepEqual(obj_4);
+
+var a = {a: 10}, b = Object.create(null);
+b.a = 10;
+
+// a.should.be.eql(b);
+
+
+[1, 2, 3].should.containDeep([3, 2]);
+// [1, 2, 3].should.containDeepOrdered([3, 2]);
+[1, 2, [3, 4]].should.containDeepOrdered([[3, 4]]);
+({a: 1, b: {c: 2}}).should.containDeepOrdered({b: {c: 2}});
+
+([1, 2]).should.be.an.Array();
+(true).should.be.an.Boolean();
+(new Date()).should.be.an.Date();
+
+function hahaFn() {
+    var args = arguments;
+    should(args).is.arguments()
+}
+hahaFn();
+
+should(null).null();
+
+(123).should.be.type('number');
+({}).should.empty();
+({a: 1, b: 2}).should.keys('a','b');
+
+({a: 1, b: 2}).should.keys('a');
+
+({a: {b: {c: {d: {e: 1}}}}}).should.propertyByPath('a', 'b', 'c');
+
+({a: {b: {c: 1}}}).should.be.eql({a: {b: {c: 1}}});
+// (1).should.be.generator();
+(new should.Assertion({a: 1})).generator();
